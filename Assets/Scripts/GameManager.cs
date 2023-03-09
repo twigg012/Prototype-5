73671d07b57fb604205d09bced92503f95be929c
twigg012 <73553932+twigg012@users.noreply.github.com>
2023-03-09 16:01:43 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+
+    private float spawnRate = 1.0f;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Spawns Targets Randomly
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    //Updates Score
+    public void UpdateScore(int scoreToAdd)
+    {
+            score += scoreToAdd;
+            scoreText.text = "Score : " + score;
+    }
+
+    //Ends the game and displays text
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    //Restarts the game
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Prototype-5
+ Prototype 5 for Unity Learn Jr Programmer
